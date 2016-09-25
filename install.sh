b242aac0f0f49b8557933f8b7f043068ebf1bbb0
Chase Rogers <chaseanthonyrogers@gmail.com>
2016-09-24 23:35:09 -0500

    feat(runcom): add .railsrc and .pryrc
    - update install.sh to include .railsrc and .pryrc

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ BOXFILES_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -sfv "$BOXFILES_ROOT/runcom/.bash_profile" ~
 ln -sfv "$BOXFILES_ROOT/runcom/.inputrc" ~
+ln -sfv "$BOXFILES_ROOT/runcom/.railsrc" ~
+ln -sfv "$BOXFILES_ROOT/runcom/.pryrc" ~
 ln -sfv "$BOXFILES_ROOT/git/.gitconfig" ~
 ln -sfv "$BOXFILES_ROOT/git/.gitignore_global" ~
 ln -sfv "$BOXFILES_ROOT/git/.gitmessage.txt" ~
