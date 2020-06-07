309498e8f0e891b315a7e40127f07cafd92cb355
Chase Rogers <chaseanthonyrogers@gmail.com>
2020-06-06 21:45:40 -0500

    remove sublime and rails setups

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # dotfiles
 
 ## Installation
-Install at your own risk!  I highly recommend making a backup of any important files before installing this.
-
 #### Clone this repository:
 
 github: `git clone https://github.com/chaserogers/dotfiles.git`
@@ -14,21 +12,3 @@ gitlab: `git clone https://gitlab.com/chaserogers/dotfiles.git`
 `source dotfiles/install.sh`
 
 That's it!  You're good to go.  Feel free to fork this repository and customize it as you see fit.
-
-## Sublime Text 3 configuration
-In order for the Sublime settings to take effect you'll need to install Sublime Text 3 and Package Control.  The instructions for installing Sublime Text 3 can be found [here] (https://www.sublimetext.com/3).  The Package Control instructions can be found [here] (https://packagecontrol.io/installation).
-
-If you'd like to omit the Sublime Text portion of this repo entirely, delete the sublime folder and lines 13 and 14 from the install.sh file before running the install script.
-
-## iTerm configuration
-This task can be accomplished from your iTerm Preferences window.  Here is a useful article on how to sync your iTerm preferences with dotfiles:  [http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/](http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/).
-
-## Rails template configuration
-To configure your rails_template to create a new repository on github and push your project to that repository on initialize, follow these steps:
-
-- navigate to dotfiles/rails/.rails_template.rb
-- uncomment lines 51-54
-- change chaserogers to your github username on line 51
-- save your changes
-
-An example application generated with this template can be found [here] (https://github.com/chaserogers/rails_template_example).
--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 DOTFILES_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Install osx config
+source "$DOTFILES_ROOT/mac/osx.sh"
+
+# Install Homebrew and packages
+source "$DOTFILES_ROOT/package/brew.sh"
+
+# Symlink vim package and config folders
+rm -rf ~/.vim
+ln -sfnv "$DOTFILES_ROOT/vim/.vim" ~/.vim
+
+# Install vscode
+rm -rf ~/Library/Application\ Support/Code/User/settings.json
+ln -sfv "$DOTFILES_ROOT/vscode/user_settings.json" ~/Library/Application\ Support/Code/User/settings.json
+
 # Symlink dotfiles
 ln -sfv "$DOTFILES_ROOT/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_ROOT/runcom/.inputrc" ~
@@ -9,32 +23,12 @@ ln -sfv "$DOTFILES_ROOT/runcom/.pryrc" ~
 ln -sfv "$DOTFILES_ROOT/runcom/.ripgreprc" ~
 ln -sfv "$DOTFILES_ROOT/runcom/.psqlrc" ~
 ln -sfv "$DOTFILES_ROOT/runcom/.jshintrc" ~
-ln -sfv "$DOTFILES_ROOT/rails/.railsrc" ~
-ln -sfv "$DOTFILES_ROOT/rails/.rails_template.rb" ~
-ln -sfv "$DOTFILES_ROOT/rails/.rspec" ~
 ln -sfv "$DOTFILES_ROOT/git/.gitconfig" ~
 ln -sfv "$DOTFILES_ROOT/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_ROOT/git/.gitmessage.txt" ~
 ln -sfv "$DOTFILES_ROOT/git/.pull_request_template.md" ~
 ln -sfv "$DOTFILES_ROOT/git/.issue_template.md" ~
 ln -sfv "$DOTFILES_ROOT/vim/.vimrc" ~
-
-rm -rf ~/Library/Application\ Support/Code/User/settings.json
-ln -sfv "$DOTFILES_ROOT/vscode/user_settings.json" ~/Library/Application\ Support/Code/User/settings.json
-
-# Symlink vim package and config folders
-rm -rf ~/.vim
-ln -sfnv "$DOTFILES_ROOT/vim/.vim" ~/.vim
-
-# Symlink preferences for Sublime Text 3
-rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -sfnv "$DOTFILES_ROOT/sublime/User" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# Install osx config
-# source "$DOTFILES_ROOT/mac/osx.sh"
-
-# Install Homebrew and packages
-# source "$DOTFILES_ROOT/package/brew.sh"
 
 # Hush terminal login messages
 touch .hushlogin
